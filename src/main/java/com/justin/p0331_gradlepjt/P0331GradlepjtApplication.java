45d25bda75fea2410fb45a3309d391b80e8cc7f9
unknown <scentwmn@gmail.com>
2022-03-31 16:32:18 +0900

    Initial commit

---
+++ src/main/java/com/justin/p0331_gradlepjt/P0331GradlepjtApplication.java
@@ -0,0 +1,15 @@
+package com.justin.p0331_gradlepjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P0331GradlepjtApplication {
+
+	public static void main(String[] args) {
+		System.out.println("START");
+		SpringApplication.run(P0331GradlepjtApplication.class, args);
+		System.out.println("END");
+	}
+
+}
---
+++ src/test/java/com/justin/p0331_gradlepjt/P0331GradlepjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justin.p0331_gradlepjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P0331GradlepjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
